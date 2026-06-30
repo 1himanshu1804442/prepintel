caa68efb37bb45918e503d73b65f2946177d4b3f
Himanshu Yadav <hy180444@gmail.com>
2026-06-30 20:16:39 +0530

    feat: remove LandingPage and set Dashboard as default root homepage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LandingPage from './LandingPage';
 import Dashboard from './Dashboard';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LandingPage />} />
+        <Route path="/" element={<Dashboard />} />
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
